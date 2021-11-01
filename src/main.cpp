9337c5cb48220559aeb9ff793dc1b2f9f5411ca3
Matteo Razzanelli <matteo.razzanelli89@gmail.com>
2021-11-01 19:33:50 +0100

    refactor: setup infrastructure for simulations

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,7 @@
 #include "utils/utils.h"
 #include "simulator/simulator.h"
 #include "control/pid.h"
-#include "control/control.h"
-// #include "model/vehicle.h"
+
 #include "matplotlibcpp.h"
 
 namespace plt = matplotlibcpp;
@@ -17,28 +16,54 @@ int main(int argc, char *argv[]) {
   // Define the simulator object
   simulator::Simulator simulation(config);
 
-  // Define all possible vehicles and controllers
-  vehicle::Unicycle *unicycle = new vehicle::Unicycle(config.simulation.dt);
-  vehicle::Bicycle *bicycle = new vehicle::Bicycle(config.simulation.dt);
-  control::Pid *pid_ang = new control::Pid(config.pid_ang.kp, config.pid_ang.kd, config.pid_ang.ki, config.pid_ang.max_err, config.simulation.dt, PIDType::ANGULAR);
-  control::Pid *pid_lat = new control::Pid(config.pid_lat.kp, config.pid_lat.kd, config.pid_lat.ki, config.pid_lat.max_err, config.simulation.dt, PIDType::LATERAL);
+  // Define all possible vehicles and controller
+  control::Pid *pid_ang = new control::Pid(config.pid_ang.kp, config.pid_ang.kd, config.pid_ang.ki, 
+                                          config.pid_ang.max_integral_error, config.vehicle.max_control, 
+                                          config.simulation.dt, PIDType::ANGULAR);
+  control::Pid *pid_lat = new control::Pid(config.pid_lat.kp, config.pid_lat.kd, config.pid_lat.ki, 
+                                          config.pid_lat.max_integral_error, config.vehicle.max_control, 
+                                          config.simulation.dt, PIDType::LATERAL);
+  vehicle::Unicycle *unicycle_pid_ang = new vehicle::Unicycle(config);
+  vehicle::Unicycle *unicycle_pid_lat = new vehicle::Unicycle(config);
+  vehicle::Bicycle *bicycle_pid_ang = new vehicle::Bicycle(config);
+  vehicle::Bicycle *bicycle_pid_lat = new vehicle::Bicycle(config);
+  
   // Run a specific simulation
-  Path simulation_result = simulation.run(unicycle, pid_ang, "Posture");
-  Path simulation_result2 = simulation.run(unicycle, pid_lat, "Posture");
+  Path uni_pidang = simulation.run(unicycle_pid_ang, pid_ang, "Posture");
+  Path uni_pidlat = simulation.run(unicycle_pid_lat, pid_lat, "Posture");
+  Path bi_pidang = simulation.run(bicycle_pid_ang, pid_ang, "Posture");
+  Path bi_pidlat = simulation.run(bicycle_pid_lat, pid_lat, "Posture");
 
   // Store
-  std::vector<double> robot_x(simulation_result.size());
-  std::vector<double> robot_y(simulation_result.size());
+  std::vector<double> uni_pidang_x(config.simulation.iterations);
+  std::vector<double> uni_pidang_y(config.simulation.iterations);
+  std::vector<double> uni_pidlat_x(config.simulation.iterations);
+  std::vector<double> uni_pidlat_y(config.simulation.iterations);
+  std::vector<double> bi_pidang_x(config.simulation.iterations);
+  std::vector<double> bi_pidang_y(config.simulation.iterations);
+  std::vector<double> bi_pidlat_x(config.simulation.iterations);
+  std::vector<double> bi_pidlat_y(config.simulation.iterations);
   size_t i = 0;
-  for (const auto& simpoint : simulation_result) {
-    robot_x[i] = simulation_result[i].pose.position.x;
-    robot_y[i] = simulation_result[i].pose.position.y;
-    i ++;
+  for (size_t i = 0; i < config.simulation.iterations; i ++) {
+    uni_pidang_x[i] = uni_pidang[i].pose.position.x;
+    uni_pidang_y[i] = uni_pidang[i].pose.position.y;
+    uni_pidlat_x[i] = uni_pidlat[i].pose.position.x;
+    uni_pidlat_y[i] = uni_pidlat[i].pose.position.y;
+    bi_pidang_x[i] = bi_pidang[i].pose.position.x;
+    bi_pidang_y[i] = bi_pidang[i].pose.position.y;
+    bi_pidlat_x[i] = bi_pidlat[i].pose.position.x;
+    bi_pidlat_y[i] = bi_pidlat[i].pose.position.y;
   }
+  std::vector<double> goal_x(1,config.posture.x), goal_y(1,config.posture.y);
+  std::vector<double> start_x(1,config.vehicle.x), start_y(1,config.vehicle.y);
 
-  // plot
-  plt::named_plot("Angular_PID",robot_x, robot_y,"r--");
-  // plt::named_plot("goal",goal_x,goal_y,"o");
+  // Plot
+  plt::named_plot("Unicycle Angular PID",uni_pidang_x, uni_pidang_y,"r--");
+  plt::named_plot("Unicycle Lateral PID",uni_pidlat_x, uni_pidlat_y,"b--");
+  plt::named_plot("Bicycle Angular PID",bi_pidang_x, bi_pidang_y,"g--");
+  plt::named_plot("Bicycle Lateral PID",bi_pidlat_x, bi_pidlat_y,"m--");
+  plt::named_plot("Goal",goal_x,goal_y,"ro");
+  plt::named_plot("Start",start_x,start_y,"bo");
   plt::title("AGV position");
   plt::legend();
   plt::show();
