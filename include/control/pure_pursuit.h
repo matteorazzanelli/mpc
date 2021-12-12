f0dfe0ed92e72a20fa63a21990b91d00be41e63b
Matteo Razzanelli <matteo.razzanelli89@gmail.com>
2021-12-12 19:23:58 +0100

    refactor: optimize pure pursuit inner variables structure

--- include/control/pure_pursuit.h
+++ include/control/pure_pursuit.h
@@ -8,14 +8,15 @@ namespace control {
     private:
       double ld_;
       double k_;
-      size_t closest_index_ = 0;
-      double curvature_ = 0.0;
-      double dt_ = 0.1;
-      double last_maneuver_ = 0.0;
-      utils::types::Path path_;
+      size_t closest_index_;
+      double curvature_;
+      double last_maneuver_;
+      double max_control_;
+      double length_;
+      const utils::types::Path* ppath_;
       utils::types::PathPoint last_lokahead_point_;
     public:
-      PurePursuit(const double& ld, const double& k, const double & dt);
+      PurePursuit(const double& ld, const double& k, const double& max_control, const double& l, const double & dt);
       void init(const utils::types::Path& path);
       Control updateControl();
       void updateControlError(vehicle::Model* model, const utils::types::Pose& target);
--- src/control/pure_pursuit.cpp
+++ src/control/pure_pursuit.cpp
@@ -1,13 +1,13 @@
 #include "control/pure_pursuit.h"
 #include <glog/logging.h>
-#include <iostream>
 
 namespace control {
-  PurePursuit::PurePursuit(const double& ld, const double& k, const double & dt) : 
-    Controller(dt), ld_(ld), k_(k) {}
+  PurePursuit::PurePursuit(const double& ld, const double& k, const double& max_control, const double& l, const double & dt) : 
+    Controller(dt), ld_(ld), k_(k), max_control_(max_control), length_(l), closest_index_(0), last_maneuver_(0.0),
+    curvature_(0.0) {}
 
   void PurePursuit::init(const utils::types::Path& path) {
-    path_ = path;
+    ppath_ = &path;
     last_lokahead_point_ = path.front();
     closest_index_ = 0;
     curvature_ = 0.0;
@@ -15,15 +15,13 @@ namespace control {
   }
   
   Control PurePursuit::updateControl() {
-    double steering_angle = std::atan2(curvature_,1.0);
+    // compute steering angle: for unicycle length_ = 0
+    double steering_angle = std::atan2(length_*curvature_,1.0);
     double omega = (steering_angle - last_maneuver_) / dt_;
-    double max_omega = 0.53; // qui ci va max_control_
-    omega = fabs(omega) > max_omega ? utils::math::sign(omega)*max_omega : omega;
-    std::cout<<omega<<" "<<steering_angle<<" "<<last_maneuver_<<std::endl;
+    omega = fabs(omega) > max_control_ ? utils::math::sign(omega)*max_control_ : omega;
     // We control the (steering) angular rate: omega = v * curvature
     auto control = utils::types::Control {
       .v = 0.01,
-      // .w = omega // biciclo
       .w = 0.01 * curvature_ + omega // FIXME: omega shuould be of the previous step not current
     };
     last_maneuver_ = steering_angle;
@@ -37,11 +35,10 @@ namespace control {
     // vinesmsuic.github.io/2020/09/29/robotics-purepersuit
     utils::types::Pose robot_pose = model->getState();
     // Find the path point closest to the vehicle
-    const utils::types::PathPoint* start = utils::path::closestPoint(closest_index_, robot_pose.position, path_);
+    const utils::types::PathPoint* start = utils::path::closestPoint(closest_index_, robot_pose.position, *ppath_);
     // Find the lookahead point
-    for(size_t i = closest_index_; i < path_.size()-1; i ++){
-      const utils::types::PathPoint* goal = utils::path::lookaheadPoint(path_.at(i), path_.at(i+1), robot_pose.position, path_, adaptive_ld, last_lokahead_point_);
-      // std::cout<<goal->pose.position.x<<" "<<goal->pose.position.y<<std::endl;
+    for(size_t i = closest_index_; i < ppath_->size()-1; i ++){
+      const utils::types::PathPoint* goal = utils::path::lookaheadPoint(ppath_->at(i), ppath_->at(i+1), robot_pose.position, *ppath_, adaptive_ld, last_lokahead_point_);
       if(goal) {
         last_lokahead_point_ = *goal;
         break;
@@ -57,27 +54,8 @@ namespace control {
     double side = - std::sin(robot_pose.heading) * (last_lokahead_point_.pose.position.x - robot_pose.position.x)
                   + std::cos(robot_pose.heading) * (last_lokahead_point_.pose.position.y - robot_pose.position.y);
     double sign = static_cast<double>(utils::math::sign(side));
-    // std::cout<<sign<<" "<<side<<" "<<x<<std::endl;
     
     // Calculate the curvature of the arc to the lookahead point
     curvature_ = sign * (2.0 * x / std::pow(adaptive_ld,2)); // = 1/R
-    // double L = 1.0;
-    // double steering_angle_rate = ((L*curvature_/x) / (1+std::pow(L*curvature_,2))) * dt_ ;
-
-    // std::cout<<curvature_<<" ";
-
-    // Alternative
-    // double desired_yaw = atan2((last_lokahead_point_.pose.position.y - robot_pose.position.y),(last_lokahead_point_.pose.position.x - robot_pose.position.x));
-    // double diff_orientation = utils::math::shortestAngularDistance(robot_pose.heading,desired_yaw);
-    // curvature_ = std::atan2(2.0 * std::sin(diff_orientation) / adaptive_ld, 1.0);
-    // std::cout<<curvature_<<" "<<steering_angle_rate<<std::endl;
-    // curvature_ = steering_angle_rate;
-
-    // R    = ld/(2*sin(alpha))
-    // 1/R  = (2*sin(alpha))/ld = (2*x)/(ld**2)
-    //delta = atan2(L/R) = atan2((L*2*x)/(ld*ld)) = atan2(ld*curvature_)
-
-    // If control the delta:
-    // delta = std::min( std::atan2(2 * x * L / ld*ld, 1.0), delta_max_ );
   }
 }
