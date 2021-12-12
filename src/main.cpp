929107200eaa0fec456c5f675df9805526b59957
Matteo Razzanelli <matteo.razzanelli89@gmail.com>
2021-12-12 19:24:55 +0100

    refactor: separate path operations from simulations

--- src/main.cpp
+++ src/main.cpp
@@ -17,69 +17,37 @@ int main(int argc, char *argv[]) {
   // Define the simulator object
   simulator::Simulator simulation(config);
 
-  // Define all possible vehicles and controller
-  control::Pid *pid_ang = new control::Pid(config.pid_ang.kp, config.pid_ang.kd, config.pid_ang.ki, config.pid_ang.max_integral_error, config.vehicle.max_control, config.simulation.dt, PIDType::ANGULAR);
-  control::Pid *pid_lat = new control::Pid(config.pid_lat.kp, config.pid_lat.kd, config.pid_lat.ki, config.pid_lat.max_integral_error, config.vehicle.max_control, config.simulation.dt, PIDType::LATERAL);
-  control::PurePursuit *pp = new control::PurePursuit(config.pure_pursuit.ld, config.pure_pursuit.k, config.simulation.dt);
+  // Define all possible controller
+  control::Pid *pid_ang_uni = new control::Pid(config.pid_ang_uni.kp, config.pid_ang_uni.kd, config.pid_ang_uni.ki, config.pid_ang_uni.max_integral_error, config.vehicle.max_control, config.simulation.dt, PIDType::ANGULAR);
+  control::Pid *pid_lat_uni = new control::Pid(config.pid_lat_uni.kp, config.pid_lat_uni.kd, config.pid_lat_uni.ki, config.pid_lat_uni.max_integral_error, config.vehicle.max_control, config.simulation.dt, PIDType::LATERAL);
+  control::Pid *pid_ang_bi = new control::Pid(config.pid_ang_bi.kp, config.pid_ang_bi.kd, config.pid_ang_bi.ki, config.pid_ang_bi.max_integral_error, config.vehicle.max_control, config.simulation.dt, PIDType::ANGULAR);
+  control::Pid *pid_lat_bi = new control::Pid(config.pid_lat_bi.kp, config.pid_lat_bi.kd, config.pid_lat_bi.ki, config.pid_lat_bi.max_integral_error, config.vehicle.max_control, config.simulation.dt, PIDType::LATERAL);
+  control::PurePursuit *pp_uni = new control::PurePursuit(config.pure_pursuit.ld, config.pure_pursuit.k, config.vehicle.max_control, 0.0, config.simulation.dt);
+  control::PurePursuit *pp_bi = new control::PurePursuit(config.pure_pursuit.ld, config.pure_pursuit.k, config.vehicle.max_control, config.vehicle.length, config.simulation.dt);
 
-  // valid only for posture mission
-  utils::types::Path path;
-  utils::types::Position start = utils::types::Position {
-    .x = config.vehicle.x,
-    .y = config.vehicle.y
-  };
-  utils::types::Position end = utils::types::Position {
-    .x = config.posture.x,
-    .y = config.posture.y
-  };
-  path.push_back(utils::types::PathPoint {
-    .pose = utils::types::Pose {
-      .position = utils::types::Position {
-        .x = config.vehicle.x,
-        .y = config.vehicle.y
-      },
-      .heading = config.vehicle.heading,
-      .steering_angle = config.vehicle.steering_angle
-    },
-    .s = 0.0,
-    .k = 0.0
-  });
-  path.push_back(utils::types::PathPoint {
-    .pose = utils::types::Pose {
-      .position = utils::types::Position {
-        .x = config.posture.x,
-        .y = config.posture.y
-      },
-      .heading = config.posture.heading,
-      .steering_angle = 0.0
-    },
-    .s = utils::math::eulerDistance(start, end),
-    .k = 0.0
-  });
-  pp->init(path);
-
+  // Define all possible vehicles
   vehicle::Unicycle *unicycle_pid_ang = new vehicle::Unicycle(config);
   vehicle::Unicycle *unicycle_pid_lat = new vehicle::Unicycle(config);
   vehicle::Unicycle *unicycle_pp = new vehicle::Unicycle(config);
   vehicle::Bicycle *bicycle_pid_ang = new vehicle::Bicycle(config);
   vehicle::Bicycle *bicycle_pid_lat = new vehicle::Bicycle(config);
   vehicle::Bicycle *bicycle_pp = new vehicle::Bicycle(config);
+
+  // first point is always robot initial pose
+  utils::types::Path path = utils::path::toPath({
+    utils::types::Position{config.vehicle.x, config.vehicle.y},
+    utils::types::Position{config.posture.x, config.posture.y}
+  });
+  pp_uni->init(path);
+  pp_bi->init(path);
   
   // Run a specific simulation
-  Path uni_pidang = simulation.run(unicycle_pid_ang, pid_ang, "Posture");
-  Path uni_pidlat = simulation.run(unicycle_pid_lat, pid_lat, "Posture");
-  Path bi_pidang = simulation.run(bicycle_pid_ang, pid_ang, "Posture");
-  Path bi_pidlat = simulation.run(bicycle_pid_lat, pid_lat, "Posture");
-  std::cout<<"##################### UNI"<<std::endl;
-  Path uni_pp = simulation.run(unicycle_pp, pp, "Posture");
-  std::cout<<"--------------------------------------"<<std::endl;
-  std::cout<<"--------------------------------------"<<std::endl;
-  std::cout<<"--------------------------------------"<<std::endl;
-  std::cout<<"--------------------------------------"<<std::endl;
-  std::cout<<"--------------------------------------"<<std::endl;
-  std::cout<<"##################### BI"<<std::endl;
-  pp->init(path);
-  Path bi_pp = simulation.run(bicycle_pp, pp, "Posture");
+  Path uni_pidang = simulation.run(unicycle_pid_ang, pid_ang_uni, "Posture");
+  Path uni_pidlat = simulation.run(unicycle_pid_lat, pid_lat_uni, "Posture");
+  Path bi_pidang = simulation.run(bicycle_pid_ang, pid_ang_bi, "Posture");
+  Path bi_pidlat = simulation.run(bicycle_pid_lat, pid_lat_bi, "Posture");
+  Path uni_pp = simulation.run(unicycle_pp, pp_uni, "Posture");
+  Path bi_pp = simulation.run(bicycle_pp, pp_bi, "Posture");
 
   // Store
   std::vector<double> uni_pidang_x(config.simulation.iterations);
@@ -113,10 +81,10 @@ int main(int argc, char *argv[]) {
   std::vector<double> start_x(1,config.vehicle.x), start_y(1,config.vehicle.y);
 
   // Plot
-  // plt::named_plot("Unicycle Angular PID",uni_pidang_x, uni_pidang_y,"r--");
-  // plt::named_plot("Unicycle Lateral PID",uni_pidlat_x, uni_pidlat_y,"g--");
-  // plt::named_plot("Bicycle Angular PID",bi_pidang_x, bi_pidang_y,"b--");
-  // plt::named_plot("Bicycle Lateral PID",bi_pidlat_x, bi_pidlat_y,"y--");
+  plt::named_plot("Unicycle Angular PID",uni_pidang_x, uni_pidang_y,"r--");
+  plt::named_plot("Unicycle Lateral PID",uni_pidlat_x, uni_pidlat_y,"g--");
+  plt::named_plot("Bicycle Angular PID",bi_pidang_x, bi_pidang_y,"b--");
+  plt::named_plot("Bicycle Lateral PID",bi_pidlat_x, bi_pidlat_y,"y--");
   plt::named_plot("Unicycle Pure Pursuit",uni_pp_x, uni_pp_y,"m--");
   plt::named_plot("Bicycle Pure Pursuit",bi_pp_x, bi_pp_y,"c--");
   plt::named_plot("Goal",goal_x,goal_y,"ro");
